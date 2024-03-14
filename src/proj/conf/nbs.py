942654b447139788a04a011e87f56800744b86fb
Milton Pividori <miltondp@gmail.com>
2024-03-14 17:13:35 -0600

    proj/conf: add nbs module

---
+++ src/proj/conf/nbs.py
@@ -0,0 +1,13 @@
+"""
+Contains configuration entries related to Jupyter Notebooks.
+"""
+
+# when a notebook is run from the command line and it finished successfully, it
+# indicates whether the original notebook should be overridden with the new one
+RUN_OVERRIDE = 1
+
+# indicates whether notebook run with papermill should use a custom kernel
+# use this option if running several notebooks in the same node (such as in a cluster).
+# This is necessary because otherwise Jupyter's selects the same ports and mess things
+# up.
+PAPERMILL_RUN_WITH_CUSTOM_KERNEL = 0
