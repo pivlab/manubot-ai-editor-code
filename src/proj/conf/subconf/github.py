f6ca11106804c16a3f8e7faf0a2a6ec56dcdf457
Milton Pividori <miltondp@gmail.com>
2024-03-14 18:11:44 -0600

    conf/subconf: fixes

--- src/proj/conf/subconf/github.py
+++ src/proj/conf/subconf/github.py
@@ -2,7 +2,10 @@
 Contains configuration to use GitHub API.
 """
 import os
+from pathlib import Path
 
-from proj.conf import common
+from proj.conf.common import ENV_PREFIX as __ENV_PREFIX
 
-API_TOKEN = os.environ.get(f"{common.ENV_PREFIX}GITHUB_AUTH_TOKEN")
+__ENV_PREFIX += f"{Path(__file__).stem.upper()}_"
+
+API_TOKEN = os.environ.get(f"{__ENV_PREFIX}AUTH_TOKEN")
